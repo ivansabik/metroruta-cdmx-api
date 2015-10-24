4b0b4f6b7df46eb2f512a710d12dcdc402dc7603
ivan <ivan.rodriguez.o@gmail.com>
2015-10-23 21:36:19 -0500

    tests unitarios, travis

---
+++ test/unit_test.js
@@ -0,0 +1,64 @@
+var assert = require('assert');
+
+describe('Tests unitarios metroruta df api', function(){
+  it('construir lineas con estaciones', function(){
+    assert.equal(false, true);
+  });
+  
+  it('calcular ruta mas cora', function(){
+    assert.equal(false, true);
+  });
+  
+  it('construir grafo del metro', function(){
+    assert.equal(false, true);
+  });
+  
+  it('buscar estacion', function(){
+    assert.equal(false, true);
+  });
+  
+  it('buscar linea por id', function(){
+    assert.equal(false, true);
+  });
+  
+  it('buscar estaciones cercanas', function(){
+    assert.equal(false, true);
+  });
+  
+  it('calcular distancia entre dos puntos laton', function(){
+    assert.equal(false, true);
+  });
+  
+  it('convertir a radianes', function(){
+    assert.equal(false, true);
+  });
+  
+  it('determinar direccion basado en estaciones', function(){
+    assert.equal(false, true);
+  });
+  
+  it('construir puntos de ruta', function(){
+    assert.equal(false, true);
+  });
+  
+  it('construir ruta', function(){
+    assert.equal(false, true);
+  });
+  
+  it('calcular tiempo de ruta con base en distancia entre estaciones', function(){
+    assert.equal(false, true);
+  });
+  
+  it('calcular velocidad promedio', function(){
+    assert.equal(false, true);
+  });
+  
+  it('calcular tiempo entre paradas', function(){
+    assert.equal(false, true);
+  });
+  
+  it('calcular numero de estaciones en toda la red de metro', function(){
+    assert.equal(false, true);
+  });
+  
+})
