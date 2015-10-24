65b34b20e4c4e872a46c041186ec865172db4884
ivan <ivan.rodriguez.o@gmail.com>
2015-10-23 23:45:23 -0500

    tungus empezando mongoose

--- db_setup.js
+++ db_setup.js
@@ -1,1 +1,45 @@
 // Llena bd con lineas y estaciones
+var tungus = require('tungus')
+, mongoose = require('mongoose')
+, lineasJs = require('./setup_data/lineas').lineasJs;
+
+mongoose.connect('tingodb:///tmp/')
+
+var Schema = mongoose.Schema;
+
+var lineaSchema = Schema({
+    nombre: String,
+    colorHex: String,
+    iconoBase64: String,
+    estaciones: [{
+      nombre: String,
+      latitud: Number,
+      longitud: Number,
+      iconoBase64: String,
+  }]
+})
+
+var Linea = mongoose.model('Linea', lineaSchema);
+
+// Bulk se implementa en la v4 de mongoose pero tungus usa 3
+// http://stackoverflow.com/questions/10266512/how-can-i-save-multiple-documents-concurrently-in-mongoose-node-js
+
+var total = lineasJs.length
+  , result = [];
+
+function _saveAll() {
+  var lineaJs = lineasJs.pop();
+  var linea = new Linea({
+    nombre: lineaJs.nombre,
+    colorHex: lineaJs.colorHex,
+    iconoBase64: lineaJs.icono
+  });
+  
+  linea.save(function(err, saved){
+    if (err) throw err;
+    result.push(saved[0]);
+    if (--total) _saveAll();
+  })
+}
+
+_saveAll();
